5d1e3c089d2d06bf6018d0a4d4aba5be4a1aae1b
Eagle0283 <86646287+Eagle0283@users.noreply.github.com>
2021-07-02 09:50:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function compute()
 {
     p = document.getElementById("principal").value;
-    
+    a = document.getElementById("Amount").value;
+    b = document.getElementById("Years").value;
+    document.getElementById("result").innerHTML = a * p * b / 100;
 }
         
