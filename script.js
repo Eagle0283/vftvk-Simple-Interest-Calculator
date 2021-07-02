f20b5ac02df169235ce752f485c17fad018d7a36
Eagle0283 <86646287+Eagle0283@users.noreply.github.com>
2021-07-02 17:58:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,9 @@
-let rangeInput = document.querySelector(".range-input input");
-let rangeValue = document.querySelector(".range-input .value div");
-
-let start = parseFloat(rangeInput.min);
-let end = parseFloat(rangeInput.max);
-let step = parseFloat(rangeInput.step);
-
-for(let i=start;i<=end;i+=step){
-  rangeValue.innerHTML += '<div>'+i+'</div>';
-}
-
-rangeInput.addEventListener("input",function(){
-  let top = parseFloat(rangeInput.value)/step * -40;
-  rangeValue.style.marginTop = top+"px";
-});
-
 function compute()
 {
     p = document.getElementById("principal").value;
     a = document.getElementById("Amount").value;
     b = document.getElementById("Years").value;
-    document.getElementById("result").innerHTML = a * p * b / 100;
+    c = "You will receive "
+    document.getElementById("result").innerHTML =c + (a * p * b / 100);
+    
 }
